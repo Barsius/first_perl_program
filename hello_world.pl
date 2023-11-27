e1add412f6e94141add250fc0ab08a969af2578d
Menshykov, Ivan [PS] <ivan.menshykov@cengage.com>
2023-11-27 22:51:20 +0100

    Delete unnecessary files

--- hello_world.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-
-print "Content-type: text/html\n\n";
-print "<html><body>";
-print "<h1>Hello from Perl!</h1>";
-print "</body></html>";
--- test.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-use strict;
-use warnings FATAL => 'all';
-use DBI;
-use DBHandler;
-
-my %users = DBHandler->getAllUsers;
-print %users;
